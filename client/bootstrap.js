296639859ba79732f187f4b15fc56513d02c2db5
Hearty Oh <heartyoh@hatiolab.com>
2019-06-18 14:59:43 +0900

    v0.0.3

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -2,17 +2,17 @@ import { UPDATE_EXTENSION } from '@things-factory/export-base'
 import { store } from '@things-factory/shell'
 import { Parser } from 'json2csv'
 
-function jsonToCsv(params) {
-  const data = typeof params.data == 'function' ? params.data.call() : params.data
+function jsonToCsv({ extension, data, name }) {
+  const records = typeof data == 'function' ? data.call() : data
 
-  const fields = Object.keys(data[0])
+  const fields = Object.keys(records[0])
   const parser = new Parser({ fields })
-  const csv = parser.parse(params.data)
+  const csv = parser.parse(records)
 
   const link = document.createElement('a')
 
   link.setAttribute('href', encodeURI('data:text/csv;charset=utf-8,' + csv))
-  link.setAttribute('download', `${params.name}.csv`)
+  link.setAttribute('download', `${name}.csv`)
 
   document.body.appendChild(link)
   link.click()
